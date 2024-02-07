ff7c8d7e6286f3ee36173d66bc31728406abac0a
glib05 <119256208+glib05@users.noreply.github.com>
2024-02-07 13:01:35 +0200

    f

--- Lab1/Import-export.py
+++ Lab1/Import-export.py
@@ -10,31 +10,30 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
 
+
 #1
-df=pd.read_excel("Lab1\stat.xls",skiprows=6,skipfooter=3)
+df=pd.read_excel("Lab1/stat.xls",skiprows=6,skipfooter=3)
 # print(df.head)
 saldo=df["Unnamed: 7"]
 names_saldo=df.iloc[:,[0,7]]
 # print(saldo.head)
 print(names_saldo)
+
 #2
-m=saldo.mean()
-print("mat", m)
-mode=saldo.mode()
-print("mode", mode if mode.size<saldo.size else "no mode")
-median=s.median(sorted(saldo))
+mean = saldo.mean()
+print("mean", mean)
+mode = saldo.mode()
+print("mode", mode if mode.size < saldo.size else "no mode")
+median = s.median(sorted(saldo))
 print("median",median)
-dysp=s.pvariance(saldo)
+dysp = s.pvariance(saldo)
 print("pvariance",dysp)
-pstdev=s.pstdev(saldo)
-print("pstdev",pstdev)
+std = s.pstdev(saldo)
+print("std",std)
 
 #3
 gs = GridSpec(1, 5)
 fig = plt.figure(figsize=(10, 6))
-
-
-
 def hui(plot_column_num:int, column_dict_name:str, column_dict_num:int, x_label:str, label:str, multiplier=1.0):
     ax = fig.add_subplot(gs[0, plot_column_num])
     cities_data = df.iloc[:, [0, column_dict_num]]
@@ -57,8 +56,6 @@ def hui(plot_column_num:int, column_dict_name:str, column_dict_num:int, x_label:
     ax.grid(axis='x')  # Grid lines now should be along the x-axis
     return ax
 
-
-
 # Сальдо - гістрограма
 hui(0,'Unnamed: 7',7,'Міста','Сальдо, млрд. долл',10e-7)
 
@@ -66,10 +63,16 @@ hui(0,'Unnamed: 7',7,'Міста','Сальдо, млрд. долл',10e-7)
 hui(2,'Unnamed: 1',1,'Міста','Експорт, млрд. долл',10e-7)
 
 # Імпорт - гістрограма
-hui(4,'Unnamed: 4',4,'Міста','Імпорт, млрд. долл',10e-7) 
+hui(4,'Unnamed: 4',4,'Міста','Імпорт, млрд. долл',10e-7)
 
-# 3
+plt.show()
+
+# 4
+#усе зроблено вище
 def do_dataframe(df:pd.DataFrame):
     pass
 
-plt.show()
+df.index = [df["у тому числі"]]
+df = df.iloc[:, 1:]
+df.at
+print(df.head())
